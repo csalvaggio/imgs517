6c9c2cbc32b0420b8f29a54a9d06a7fd6975829b
Carl Salvaggio <salvaggio@cis.rit.edu>
2026-01-11 11:07:11 -0500

    Added markers for LaTeX code referencing

--- code/pydantic_python_class_refactored.py
+++ code/pydantic_python_class_refactored.py
@@ -4,6 +4,7 @@ from numpy.typing import ArrayLike
 from pydantic import BaseModel, Field, ConfigDict
 from typing import ClassVar
 
+# --- BEGIN: Blackbody class ---
 class Blackbody(BaseModel):
     absolute_temperature: float = Field(
         ge=0,
@@ -28,7 +29,9 @@ class Blackbody(BaseModel):
 
     def radiance(self, wavelength: ArrayLike) -> float | np.ndarray:
         return self.exitance(wavelength) / np.pi
+# --- END: Blackbody class ---
 
+# --- BEGIN: Graybody class ---
 class Graybody(Blackbody):
     emissivity: float = Field(ge=0.0, le=1.0)
 
@@ -36,6 +39,7 @@ class Graybody(Blackbody):
 
     def exitance(self, wavelength: ArrayLike) -> float | np.ndarray:
         return self.emissivity * super().exitance(wavelength)
+# --- END: Graybody class ---
 
 if __name__ == "__main__":
     wavelength = 10
